9665dbd520680c5c3e35c20c143ee31e112b3ad0
TyrantLink <tyrantlink@pm.me>
2023-04-18 00:30:07 -0700

    removed index endpoint

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,16 @@ use rocket::serde::json::{json,Value};
 
 mod dad_bot;
 
-
-#[get("/")]
-fn index() -> String {
-	String::from("Hello, world!")
-}
-
 #[get("/dad_bot?<message>&<name>")]
-fn dad_bot_api(message: &str, name:&str) -> Value {
+fn dad_bot_api(message: &str, name:&str) -> Value
+{
 	let result = dad_bot::dad_bot(message,name);
 	json!({"response":result})
 }
 
 #[launch]
-fn rocket() -> _ {
+fn rocket() -> _
+{
 	rocket::build()
-		.mount("/", routes![index,dad_bot_api])
+		.mount("/", routes![dad_bot_api])
 }
